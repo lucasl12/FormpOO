9528c0ffd52c3900d8f867ea8361bd3d1ddc1ad1
Lucas Lopes <lucaslopes05561@gmail.com>
2018-12-01 18:23:09 -0200

    Adicionado classes formulario e pergunta

---
+++ app/src/main/java/com/example/lucas/formpoo/form/Formulario.java
@@ -0,0 +1,66 @@
+package com.example.lucas.formpoo.form;
+
+import com.example.lucas.formpoo.perguntas.Pergunta;
+
+import java.util.ArrayList;
+
+public class Formulario {
+
+
+    private String nomeFormulario;
+    private String descricaoFormulario;
+    private String dataInicio;
+    private String dataFinal;
+    private ArrayList<Pergunta> pergunta = new ArrayList <Pergunta>();
+
+
+
+
+
+    public Formulario(String nomeFormulario, String descricaoFormulario, String dataInicio, String dataFinal,
+                      ArrayList<Pergunta> pergunta) {
+        super();
+        this.nomeFormulario = nomeFormulario;
+        this.descricaoFormulario = descricaoFormulario;
+        this.dataInicio = dataInicio;
+        this.dataFinal = dataFinal;
+        this.pergunta = pergunta;
+    }
+    public String getNomeFormulario() {
+        return nomeFormulario;
+    }
+    public void setNomeFormulario(String nomeFormulario) {
+        this.nomeFormulario = nomeFormulario;
+    }
+    public String getDescricaoFormulario() {
+        return descricaoFormulario;
+    }
+    public void setDescricaoFormulario(String descricaoFormulario) {
+        this.descricaoFormulario = descricaoFormulario;
+    }
+    public String getDataInicio() {
+        return dataInicio;
+    }
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+    public String getDataFinal() {
+        return dataFinal;
+    }
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+    public ArrayList<Pergunta> getPergunta() {
+        return pergunta;
+    }
+    public void setPergunta(ArrayList<Pergunta> pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public boolean salvarFormulario(Formulario f) {
+        return true;
+    }
+}
+
+
+
